5096bd19e99d1db52f401b16f6b46eb779fbbcb1
mehdi <dehghani_m@rocketmail.com>
2023-05-10 12:52:05 +0430

    start make network util

--- app/src/main/java/com/example/covid19project/di/NetworkModule.kt
+++ app/src/main/java/com/example/covid19project/di/NetworkModule.kt
@@ -1,13 +1,17 @@
 package com.example.covid19project.di
 
+import android.content.Context
 import com.example.covid19project.api.Covid19IndiaApiService
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import okhttp3.Cache
+import okhttp3.OkHttpClient
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
-
+@ExperimentalCoroutinesApi
 val networkModule = module {
     single {
         Retrofit.Builder()
@@ -17,5 +21,18 @@ val networkModule = module {
                     Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
                 )
             )
+            .client()
     }
+}
+
+fun getOkHttpClient(context: Context): OkHttpClient {
+    val cacheSize = (5 * 1024 * 1024).toLong()
+    val myCache = Cache(context,cacheDir,context)
+
+    return OkHttpClient.Builder()
+        .cache(myCache)
+        .addInterceptor { chain ->
+            var request = chain.request()
+            request = if ()
+        }
 }
---
+++ app/src/main/java/com/example/covid19project/util/NetworkUtils.kt
@@ -0,0 +1,2 @@
+package com.example.covid19project.util
+
