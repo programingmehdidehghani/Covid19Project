c1e36aa3ce309547f323e80290a65221a9a0b606
mehdi <dehghani_m@rocketmail.com>
2023-05-10 13:34:50 +0430

    check theme device

---
+++ app/src/main/java/com/example/covid19project/util/applyTheme.kt
@@ -0,0 +1,20 @@
+package com.example.covid19project.util
+
+import android.content.res.Configuration
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import java.util.Calendar
+
+fun applyTheme(theme: Int){
+    AppCompatDelegate.setDefaultNightMode(theme)
+}
+
+fun AppCompatActivity.isDarkTheme(): Boolean{
+    return (resources.configuration.uiMode and
+            Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES)
+}
+
+fun isNight(): Boolean{
+    val currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+    return (currentHour <= 7 || currentHour >= 18)
+}
