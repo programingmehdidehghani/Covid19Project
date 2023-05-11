13aadbbb71ecf3c0154aaed2d2b313c02c7164b5
mehdi <dehghani_m@rocketmail.com>
2023-05-11 15:44:37 +0430

    make interface retrofit

--- app/src/main/java/com/example/covid19project/api/Covid19IndiaApiService.kt
+++ app/src/main/java/com/example/covid19project/api/Covid19IndiaApiService.kt
@@ -1,11 +1,17 @@
 package com.example.covid19project.api
 
+import com.example.covid19project.model.StateDetailsResponse
+import com.example.covid19project.model.StateResponse
+import retrofit2.Response
 import retrofit2.http.GET
 
 interface Covid19IndiaApiService {
 
-/*    @GET("data.json")
-    suspend fun getData():*/
+    @GET("data.json")
+    suspend fun getData(): Response<StateResponse>
+
+    @GET("v2/state_district_wise.json")
+    suspend fun getStateDistrictData(): Response<List<StateDetailsResponse>>
 
     companion object {
         const val BASE_URL = "https://api.covid19india.org/"
