fa5d25c5ba35a280c31846a9411a041da7bddd1f
mehdi <dehghani_m@rocketmail.com>
2023-05-11 16:08:11 +0430

    make response retrofit

--- app/src/main/java/com/example/covid19project/repository/NetworkBoundRepository.kt
+++ app/src/main/java/com/example/covid19project/repository/NetworkBoundRepository.kt
@@ -1,4 +1,34 @@
 package com.example.covid19project.repository
 
-abstract class NetworkBoundRepository {
+import androidx.annotation.MainThread
+import com.example.covid19project.util.State
+import kotlinx.coroutines.flow.flow
+import retrofit2.Response
+import java.lang.Exception
+
+abstract class NetworkBoundRepository<T> {
+
+    fun asFlow() = flow<State<T>> {
+
+        emit(State.loading())
+
+        try {
+            val apiResponse = fetchFromRemote()
+
+            val remotePosts = apiResponse.body()
+
+            if (apiResponse.isSuccessful && remotePosts != null) {
+                emit(State.success(remotePosts))
+            } else {
+                emit(State.error(apiResponse.message()))
+            }
+
+        } catch (e: Exception) {
+            emit(State.error("Network error! Can't get latest data."))
+            e.printStackTrace()
+        }
+    }
+
+    @MainThread
+    abstract suspend fun fetchFromRemote(): Response<T>
 }
